dbbf312ba6c96f2a2b824da8d5145bb911faeb36
Joerg <joerg.planner@fau.de>
2017-01-30 12:01:36 +0100

    Added solution for GENOM

---
+++ ICPC/solved/GENOM/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  double a,b;
+  int c, len;
+  cin >> a >> b >> c >> len;
+  vector<char> genes(len);
+  for (int i = 0; i < len; ++i) {
+    double tmp;
+    cin >> tmp;
+    if (tmp > a && tmp < b) genes[i] = 'X';
+    else if (tmp < a) genes[i] = 'a';
+    else genes[i] = 'b';
+  }
+  int last_a = -1;
+  int last_b = -1;
+  int a_count = 0;
+  int b_count = 0;
+  vector<pair<int,int>> solutions;
+  for (int i = 0; i < len; ++i) {
+    if (genes[i] == 'X') continue;
+    // A CASE
+    else if (genes[i] == 'a') {
+      if (last_a == -1) {
+        last_a = i;
+        if (last_b != -1) a_count++;
+      } else {
+        if (last_b != -1) a_count++;
+        if (b_count >= c) { // found a solution
+          solutions.push_back(make_pair(last_a, i));
+        }
+        b_count = 0;
+        last_a = i;
+      }
+    // B CASE
+    } else {
+      if (last_b == -1) {
+        last_b = i;
+        if (last_a != -1) b_count++;
+      } else {
+        if (last_a != -1) b_count++;
+        if (a_count >= c) { // found a solution
+          solutions.push_back(make_pair(last_b, i));
+        }
+        a_count = 0;
+        last_b = i;
+      }
+    }
+  }
+  // baXXXXXXXXXabab
+  // write solutions
+  for (int i = 0; i < (int)solutions.size(); ++i) {
+    int from = get<0>(solutions[i]);
+    int to = get<1>(solutions[i]);
+    for (int j = from; j <= to; ++j) {
+      cout << genes[j];
+    }
+    cout << endl;
+  }
+  return 0;
+}
